77acc27b94001840c4a019adae9b2b42216f8f92
s-andrews <simon@proteo.me.uk>
2026-09-21 11:41:04 +0100

    Take the default flex down to 2

--- nexons.py
+++ nexons.py
@@ -1278,8 +1278,8 @@ def get_options():
 
     parser.add_argument(
         "--flex","-f",
-        help="How many bases different can exon boundaries be and still merge them (default 3)",
-        default=3, 
+        help="How many bases different can exon boundaries be and still merge them (default 2)",
+        default=2, 
         type=int
     )
 
